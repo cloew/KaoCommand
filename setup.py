e2ef88de21202443cf81dc037f7a602e4cb9b823
Chris Loew <cloew123@gmail.com>
2014-11-11 10:17:08 -0700

    Specified that this requires the argparse library

--- setup.py
+++ setup.py
@@ -7,4 +7,5 @@ setup(name='kao_command',
       author_email='cloew123@gmail.com',
       #url='http://www.python.org/sigs/distutils-sig/',
       packages=['kao_command'],
+      requires=['argparse']
      )
