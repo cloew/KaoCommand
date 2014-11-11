293773106a02522bbbd01f1a16b9bfbdb8026c9e
Chris Loew <cloew123@gmail.com>
2014-11-11 09:46:48 -0700

    Added basic setup file and project package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='kao_command',
+      version='.1',
+      #description='Kao Tessur Command Framework',
+      author='Chris Loew',
+      author_email='cloew123@gmail.com',
+      #url='http://www.python.org/sigs/distutils-sig/',
+      packages=['kao_command'],
+     )
