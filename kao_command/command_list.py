e45470a8c999e71776a48f425652a27fc3982479
Chris Loew <cloew123@gmail.com>
2014-11-11 10:10:37 -0700

    Added slightly tidied up versions of the command list and command manager from PBF

---
+++ kao_command/command_list.py
@@ -0,0 +1,92 @@
+import argparse
+
+class CommandList:
+    """ Represents a Command that allows access to other Commands """
+    
+    def __init__(self, scriptName, category=None):
+        """ Initialize the Command List """
+        self.scriptName = scriptName
+        self.category = category
+        self.commands = {}
+        
+    def run(self, args):
+        """ Run the command list """
+        if len(args) > 0 and args[0] in self.commands:
+            command = self.commands[args[0]]
+            argsForNextCommand = args[1:]
+            if isinstance(command, CommandList):
+                command.run(argsForNextCommand)
+            else:
+                self.runBaseCommand(command, argsForNextCommand)
+        else:
+            self.help()
+            
+    def runBaseCommand(self, command, argsForNextCommand):
+        """ Run the command with the given arguments """
+        parser = self.buildArgParser(command)
+        arguments = parser.parse_args(argsForNextCommand)
+        command.run(arguments)
+        
+    def buildArgParser(self, command):
+        """ Return a parser """
+        parser = argparse.ArgumentParser(prog=self.buildProgramName(command))
+        command.addArguments(parser)
+        return parser
+        
+    def buildProgramName(self, command):
+        """ Return the program name for the given command """
+        programPieces = []
+        
+        if hasattr(command, 'category'):
+            programPieces.append(command.category)
+            
+        if hasattr(command, 'command'):
+            programPieces.append(command.command)
+            
+        return "{0} {1}".format(self.scriptName, " ".join(programPieces))
+        
+    def addCommand(self, category, command):
+        """ Add the Command to run for the given category """
+        self.commands[category] = command
+        
+    def getTabCompletion(self, arguments):
+        """ Return the tab completion for the given arguments """
+        if len(arguments) == 0:
+            return self.commands.keys()
+        elif arguments[0] in self.commands:
+            if hasattr(self.commands[arguments[0]], "getTabCompletion"):
+                return self.commands[arguments[0]].getTabCompletion(arguments[1:])
+            else:
+                return []
+        else:
+            return []
+        
+    def help(self):
+        """ Print the usage for the Command List """
+        print "Available {0} Categories".format(self.name)
+        commandList = self.commands.keys()
+        commandList.sort()
+        for category in commandList:
+            print "    {0:<15}{1}".format(category+":", self.commands[category].description)
+            
+            
+    def __contains__(self, category):
+        """ Return whether the Command List has the given category """
+        return category in self.commands
+        
+    def __getitem__(self, key):
+        """ Return the command at the given key """
+        return self.commands[key]
+        
+    @property
+    def description(self):
+        """ Return the description for the Command List """
+        return "List of {0} Commands".format(self.name)
+        
+    @property
+    def name(self):
+        """ Return the name of the Command List """
+        name = self.category
+        if name is None:
+            name = self.scriptName
+        return name.capitalize()
