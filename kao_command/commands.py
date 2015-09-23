cf406bf18ff89831b7cda373ec88b71c66f45438
Chris Loew <cloew123@gmail.com>
2015-09-23 15:50:08 -0600

    Added helper class to facilitate loading the initial Command Lists

---
+++ kao_command/commands.py
@@ -0,0 +1,26 @@
+from .cmd_config import CmdConfig
+from .command_list import CommandList
+
+class Commands:
+    """ Helper class to facilitate easy specification of Commands """
+    
+    def __init__(self, rootPackage, commandTree):
+        """ Initialize with the Root Package the Commands are specified relative to and the Command Tree """
+        self.rootPackage = rootPackage
+        self.commandTree = commandTree
+        
+    def buildCommandLists(self):
+        """ Build the command lists """
+        return self.buildCommandList([], self.commandTree)
+        
+    def buildCommandList(self, args, commandTree)
+        """ Return the Command List for the Command Tree """
+        commands = {}
+        for arg, value in commandTree.items():
+            newArgs = args + [arg]
+            if type(value) == str:
+                commands[arg] = CmdConfig(" ".join(newArgs), "{0}.{1}".format(self.rootPackage, value))
+            else:
+                commands[arg] = self.buildCommandList(self, newArgs, value)
+                
+        return CommandList(" ".join(args), commands)
