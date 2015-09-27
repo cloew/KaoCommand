9c45368f7c33ecef0d1b41293cb4123fe087c90a
Chris Loew <cloew123@gmail.com>
2015-09-27 14:09:47 -0600

    Version Bump

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
 setup(name='kao_command',
-      version='0.2.0',
+      version='0.3.0',
       #description='Kao Tessur Command Framework',
       author='Chris Loew',
       author_email='cloew123@gmail.com',
       #url='http://www.python.org/sigs/distutils-sig/',
-      packages=['kao_command'],
+      packages=['kao_command', 'kao_command.args'],
       requires=['argparse', 'kao_modules', 'kao_decorators']
      )
